c51cca8d802ff81b59c206365e917c6225593fc4
karishmashreesoftic <karishma.shreesoftic@gmail.com>
2022-07-01 16:05:58 +0530

    First Commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require("cors")
+const path = require("path")
 const bodyParser = require('body-parser');
 const userRouter = require("./routes/userRoutes");
 const ownerRouter = require("./routes/ownerRoutes");
